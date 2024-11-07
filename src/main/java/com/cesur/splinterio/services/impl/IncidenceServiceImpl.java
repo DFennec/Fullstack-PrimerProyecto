02703359e9477a4fb1c849ff52defb9fe79ec64a
DFennec <alg888999@gmail.com>
2024-11-07 18:53:35 +0100

    [BK] 7-11-2024 changes

--- src/main/java/com/cesur/splinterio/services/impl/IncidenceServiceImpl.java
+++ src/main/java/com/cesur/splinterio/services/impl/IncidenceServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.cesur.splinterio.models.Incidence;
 import com.cesur.splinterio.models.User;
-import com.cesur.splinterio.models.dtos.IncienceDTO;
+import com.cesur.splinterio.models.dtos.IncidenceDto;
 import com.cesur.splinterio.repositories.IncidenceRepository;
 import com.cesur.splinterio.repositories.UserRepository;
 import com.cesur.splinterio.services.IncidenceService;
@@ -25,12 +25,12 @@ public class IncidenceServiceImpl implements IncidenceService {
     @Override
     public List<Incidence> getIncidencesByUserName(String username) {
         User user = userRepository.getUserByEmail(username).get();
-        List<Incidence> incidencesByUser = incidenceRepository.findByUser(user).get();
+        List<Incidence> incidencesByUser = incidenceRepository.findByUser(user.getId()).get();
         return incidencesByUser;
     }
 
     @Override
-    public void storeIncidence(IncienceDTO datos) {
+    public void storeIncidence(IncidenceDto datos) {
         Optional<User> user = userRepository.getUserByEmail(datos.getUserCreated());
         if(user.isPresent()){
             Incidence incidence = new Incidence();
@@ -45,12 +45,48 @@ public class IncidenceServiceImpl implements IncidenceService {
 
     @Override
     public void deleteIncidence(Long id) {
-        throw new UnsupportedOperationException("Unimplemented method 'deleteIncidence'");
+        Optional<Incidence> incidence = incidenceRepository.findById(id);
+        if(!incidence.isEmpty()){
+            incidenceRepository.delete(incidence.get());
+        }        
     }
 
     @Override
     public List<Incidence> getAllIncidences() {
         return incidenceRepository.findAll();
+    }
+
+    @Override
+    public void updatePartialIncidence(Long id, IncidenceDto datos) {
+        Optional<Incidence> incidence = incidenceRepository.findById(id);
+        if(!incidence.isEmpty()){
+            Incidence partialIncidence = incidence.get();
+            if(datos.getDescription() != null){
+                partialIncidence.setDescription(datos.getDescription());
+            }
+            if(datos.getPriority() != null){
+                partialIncidence.setPriority(datos.getPriority());
+            }
+            if(datos.getScope() != null){
+                partialIncidence.setScope(datos.getScope());
+            }
+            partialIncidence.setUpdateAt(LocalDateTime.now());
+            incidenceRepository.save(partialIncidence);
+        }
+    }
+
+    @Override
+    public void updateAllIncidence(Incidence datos) {
+        Optional<Incidence> optionalEntity = incidenceRepository.findById(datos.getId());
+        if(!optionalEntity.isEmpty()){
+            Incidence fullIncidence = optionalEntity.get();
+            fullIncidence.setDescription(datos.getDescription());
+            fullIncidence.setPriority(datos.getPriority());
+            fullIncidence.setScope(datos.getScope());
+            fullIncidence.setUpdateAt(LocalDateTime.now());
+            fullIncidence.setUserCreated(datos.getUserCreated());
+            incidenceRepository.save(fullIncidence);
+        }
     }
 
 }
